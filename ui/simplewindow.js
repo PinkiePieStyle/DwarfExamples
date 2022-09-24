90ade9a143acb0c09d1d90c81468312add95d4d3
PinkiePieStyle <svenw1980@gmail.com>
2022-09-24 12:52:40 +0200

    Update

--- ui/simplewindow.js
+++ ui/simplewindow.js
@@ -2,11 +2,25 @@
 const myWindow = {
     container: {
         id: "mywindow",
-        type: "window",
-        modal: false,
+        type: "window", // main container - required - "window" or "dialog" see doc->containers
         title: "Simple Window",
+        options: { //optional
+            static: false, // default: false - Windows/Dialogs are destroyed after closing otherwise use static: true
+            modal: false, // default: false
+            blocking: false, // default: false
+            moveable: true, // default: true
+            hasCloseButton: true, // default: true
+            canBeMaximized: false, // window default: true - dialog default: false
+            canBeMinimized: false, // see comment above
+        },
+        onClose: function () {
+            console.log("Before window/dialog is closed");
+        },
         onClosed: function () {
-            console.log("Window was closed");
+            console.log("Window/Dialog was closed");
+        },
+        onPositionChanged: function (pos) {
+            console.log("New Position: " + pos.x + ", " + pos.y);
         },
         elements: [
             {
